4e62c88f69c3d655a68b0396fdd1a3a30c449b61
niksparab84 <niksparab84@gmail.com>
2025-05-20 00:58:43 +0530

    Initial commit
    Initial commit

---
+++ src/main/java/com/ascendion/libraryManagementService/LibraryManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ascendion.libraryManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ascendion/library_management_service/LibraryManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascendion.library_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
